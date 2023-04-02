e8342d33f31fca0c8714ad116b040f3110fd7041
daancabo97 <danielcaicedo285@gmail.com>
2023-04-02 18:59:34 -0500

    validando chat

--- src/test/TestServer.java
+++ src/test/TestServer.java
@@ -1,35 +1,43 @@
 package test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-import com.arquitectura.net.server.ServerRunnable;
+import com.arquitectura.net.server.ServerThread;
 
-public class TestServer extends ServerRunnable {
-
-    private static final int DEFAULT_PORT = 2023;
-
-    public TestServer() {
-        super(DEFAULT_PORT);
-    }
+public class TestServer implements Runnable {
+    private int port;
+    private ServerSocket serverSocket;
 
     public TestServer(int port) {
-        super(port);
+        this.port = port;
     }
 
     @Override
-    public void connectionAccepted(Socket client) {
-    	User user = new User(client);
-    	Thread thread = new Thread(user);
-    	thread.start();
-        
-    }
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Servidor iniciado en el puerto " + port);
 
-    public static void main(String[] args) {
-        TestServer server = new TestServer();
-        Thread thread = new Thread(server);
-        
-        thread.start();
-        
-        System.out.println("hola");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+
+                ServerThread serverThread = new ServerThread(clientSocket);
+                serverThread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
